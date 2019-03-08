b727a7bd4a90945526a730df8b9180fecce868b1
Sergey Konakov <skonakov@bluip.com>
2019-03-08 23:54:10 +0400

    1. Unneeded class removed 2. Comments added

--- Konak.Common.Helpers/CompareExtensions.cs
+++ Konak.Common.Helpers/CompareExtensions.cs
@@ -10,9 +10,16 @@ namespace Konak.Common.Helpers
         [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
         static extern int memcmp(byte[] b1, byte[] b2, long count);
 
-
+        /// <summary>
+        /// Extension to compare two byte arrays
+        /// </summary>
+        /// <param name="firstArray"></param>
+        /// <param name="secondArray"></param>
+        /// <returns></returns>
         public static bool Compare(this byte[] firstArray, byte[] secondArray)
         {
+            if (firstArray == null || secondArray == null) return firstArray == secondArray;
+
             return firstArray.Length == secondArray.Length && memcmp(firstArray, secondArray, firstArray.Length) == 0;
         }
     }
--- Konak.Common.Helpers/IsEmptyExtension.cs
+++ Konak.Common.Helpers/IsEmptyExtension.cs
@@ -7,31 +7,52 @@ namespace Konak.Common.Helpers
 {
     public static class IsEmptyExtension
     {
+        /// <summary>
+        /// Extension to check if collection is empty
+        /// </summary>
+        /// <param name="param">Collection to check</param>
+        /// <returns></returns>
         public static bool IsEmpty(this ICollection param)
         {
             return (param == null || param.Count == 0);
         }
 
+        /// <summary>
+        /// Extension to check if enumerable object is empty
+        /// </summary>
+        /// <param name="param">IEnumerable object to check</param>
+        /// <returns></returns>
         public static bool IsEmpty(this IEnumerable param)
         {
             return (param == null || !param.GetEnumerator().MoveNext());
         }
 
+        /// <summary>
+        /// Extension to check if array is empty
+        /// </summary>
+        /// <param name="param">Array to check</param>
+        /// <returns></returns>
         public static bool IsEmpty(this Array param)
         {
             return (param == null || param.Length == 0);
         }
 
-        public static bool IsEmpty(this string param)
-        {
-            return IsEmpty(param, true);
-        }
-
-        public static bool IsEmpty(this string param, bool ignoreWhiteSpaces)
+        /// <summary>
+        /// Extension to check if string is empty
+        /// </summary>
+        /// <param name="param">String to check if it is empty</param>
+        /// <param name="ignoreWhiteSpaces">if string contains only whitespaces interpret it as empty</param>
+        /// <returns></returns>
+        public static bool IsEmpty(this string param, bool ignoreWhiteSpaces = true)
         {
             return ignoreWhiteSpaces ? string.IsNullOrWhiteSpace(param) : string.IsNullOrEmpty(param);
         }
 
+        /// <summary>
+        /// Extension to check if Guid is empty
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
         public static bool IsEmpty(this Guid param)
         {
             return Guid.Empty.Equals(param);
